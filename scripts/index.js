12862db3276357002fe8816bd98037e088a4255b
monshdi <dimamonch1@gmail.com>
2020-12-22 13:07:30 +0700

    Second homework

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
+import { videoPlayerInit } from './videoPlayer.js'
 import { radioPlayerInit } from './radioPlayer.js'
 import { musicPlayerInit } from './musicPlayer.js'
-import { videoPlayerInit } from './videoPlayer.js'
 
 const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
@@ -21,7 +21,6 @@ playerBtn.forEach((btn, i) => {
 })
 
 
-
+videoPlayerInit();
 radioPlayerInit();
 musicPlayerInit();
-videoPlayerInit();
